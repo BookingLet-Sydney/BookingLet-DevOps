552f6f34a6ad578b1bc2247769f8dfb1c99c8ead
Xiaoyu Fan <fxy4560654@gmail.com>
2023-01-30 19:18:03 +1100

    update vpc module

--- modules/aws_back_end/vpc/vpc.tf
+++ modules/aws_back_end/vpc/vpc.tf
@@ -1,17 +1,21 @@
-variable "vpcname" {
-  type = string
-}
-//input
-
-output "vpc_arn" {
-  value = aws_vpc.xxx.arn
-}
-//output
 
 
-resource "aws_vpc" "xxx" {
-  cidr_block = "10.0.0.0/16"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "${var.prefix}-${terraform.workspace}-vpc"
+  cidr = var.cidr
+
+  azs             = var.azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = var.enable_nat_gateway
+  enable_vpn_gateway = var.enable_vpn_gateway
+
   tags = {
-    Name = var.vpcname
+    Terraform   = "true"
+    Environment = terraform.workspace
   }
 }
+
