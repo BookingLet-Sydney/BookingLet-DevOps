08dc316beca6f3e912b986105c7f3104c450b0b3
Xiaoyu Fan <fxy4560654@gmail.com>
2023-02-15 16:35:35 +1100

    3

--- CICD/aws_front_end/scripts/echo.sh
+++ CICD/aws_front_end/scripts/echo.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 set -e
 echo "stradadasds,wocaonimamama is echo ${workspace}"
+echo "stradadasds,wocaonimamama is echo ${destroy}"
+echo "stradadasds,wocaonimamama is echo ${HOSTED_ZONE_ID}"
