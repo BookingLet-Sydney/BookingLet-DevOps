e0cdd39a50bc49269f22a0833f690bc866b8ea7d
Xiaoyu Fan <fxy4560654@gmail.com>
2023-02-15 17:08:08 +1100

    -qq

--- CICD/aws_front_end/scripts/echo.sh
+++ CICD/aws_front_end/scripts/echo.sh
@@ -2,18 +2,18 @@
 set -e
 echo "test"
 ### aws
-apt-get update && apt install python3-pip -y && pip3 install awscli --upgrade
+apt-get -qq update && apt -qq install python3-pip -y && pip3 install awscli --upgrade
 echo "aws --version"
 aws --version
 
 
 ### terraform 
-sudo apt update
-sudo apt install  software-properties-common gnupg2 curl
+sudo apt -qq update
+sudo apt -qq install  software-properties-common gnupg2 curl
 curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor > hashicorp.gpg
 
 sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/
-sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-add-repository -qq "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 
-sudo apt install terraform
+sudo apt -qq install terraform
 terraform --version
