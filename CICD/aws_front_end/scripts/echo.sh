fefc815f0fdff25d78090777eccf720a529909d7
Xiaoyu Fan <fxy4560654@gmail.com>
2023-02-15 16:39:58 +1100

    $1

--- CICD/aws_front_end/scripts/echo.sh
+++ CICD/aws_front_end/scripts/echo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-echo "stradadasds,wocaonimamama is params worksapce ${params.workspace}"
-echo "stradadasds,wocaonimamama is params ${params.destroy}"
+echo "stradadasds,wocaonimamama is params worksapce ${workspace}"
+echo "stradadasds,wocaonimamama is params ${destroy}"
 echo "stradadasds,wocaonimamama is environemtnal ${HOSTED_ZONE_ID}"
--- CICD/aws_front_end/scripts/workspace.sh
+++ CICD/aws_front_end/scripts/workspace.sh
@@ -2,4 +2,4 @@
 set -e
 terraform workspace list
 echo "heheheheh--$1"
-terraform workspace select ${destroy}
+terraform workspace select $1
